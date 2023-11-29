4574be31f55020595d0668e0983d5b30fa7e2e45
bbaransel <baran9932@gmail.com>
2023-11-29 16:20:28 +0300

    html adjustment

--- Odev1/app.js
+++ Odev1/app.js
@@ -2,4 +2,4 @@
 let second = prompt("Lütfen saniye cinsinden bir değer giriniz.");
 let minute = parseInt(second / 60);
 let second2 = second % 60;
-console.log(`${minute} dakika ${second2} saniye `);
+alert(`${minute} dakika ${second2} saniye `);
