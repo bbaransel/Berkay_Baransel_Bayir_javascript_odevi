663f738f7081dbcd2ac0f187e09bfce2dd113988
bbaransel <baran9932@gmail.com>
2023-12-01 23:31:45 +0300

    Odev8 shortened

--- Odev8/app.js
+++ Odev8/app.js
@@ -4,28 +4,39 @@ console.log(`url içerisinde "${url.split('').length}" adet karakter vardır.`);
 console.log(`Ders adı "${dersAdi.split(' ').length}" adet kelimeden oluşmaktadır.`);
 
 // -url https ile başlıyor mu ?
-let https = "https"
-let counter = 0;
-let urlArray = url.split('');
-let httpsArray = https.split('');
-let num = httpsArray.length;
-for (let i = 0; i < num; i++) {
-    if (urlArray[i] == httpsArray[i]) {
-        counter++;
-    };
-};
-if (counter = https.split("").length) {
-    console.log("Evet");
+
+// Önce kendim döngü kurarak denedim.
+
+// let https = "https"
+// let counter = 0;
+// let urlArray = url.split('');
+// let httpsArray = https.split('');
+// let num = httpsArray.length;
+// for (let i = 0; i < num; i++) {
+//     if (urlArray[i] == httpsArray[i]) {
+//         counter++;
+//     };
+// };
+// if (counter = https.split("").length) {
+//     console.log("Evet");
+// } else {
+//     console.log("Hayır");
+// };
+
+if (url.startsWith("https")) {
+    console.log("Evet 'https' ile başlıyor")
 } else {
-    console.log("Hayır");
-};
+    console.log("Hayır 'https' ile başlamıyor.")
+}
+
+
 
 // Ders adı içinde "Eğitimi kelimesi geçiyor mu"
 if (dersAdi.split(' ').includes("Eğitimi")) console.log(`Ders adı içinde "Eğitimi" kelimesi geçmektedir.`);
 
 //  -url ve ders adını kullanarak aşağıdaki string ifadeyi oluşturun:
 // https://www.wissenakademie.com/fullstack-web-gelistirme-egitimi
-
+let urlArray = url.split('');
 urlArray.splice(urlArray.indexOf('&'), 5);
 urlArray.push("/");
 let newUrl = urlArray.join("");
